9286cef19766fac4ae445348808c7592d9dceefc
Omar Bernardot <omarbernardot@gmail.com>
2024-11-17 12:08:00 +0000

    Updated gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,64 @@
-const { src, dest, series, parallel } = require("gulp");
-const uglify = require("gulp-uglify");
-const postcss = require("gulp-postcss");
-const cssnano = require("cssnano");
-const rename = require("gulp-rename");
-const replace = require("gulp-replace");
+const uglifyjs = require("uglify-es");
+const gulp = require("gulp");
+const composer = require("gulp-uglify/composer");
+const pump = require("pump");
+const zip = require("gulp-zip");
 
-// We'll use a dynamic import for gulp-zip
-let zip;
-import("gulp-zip").then((module) => {
-  zip = module.default;
+const minify = composer(uglifyjs, console);
+
+gulp.task("compress", function (cb) {
+  const options = {
+    mangle: true,
+    compress: {
+      dead_code: true,
+      drop_debugger: true,
+      conditionals: true,
+      evaluate: true,
+      booleans: true,
+      loops: true,
+      unused: true,
+      hoist_funs: true,
+      keep_fargs: false,
+      hoist_vars: true,
+      if_return: true,
+      join_vars: true,
+      cascade: true,
+      side_effects: true,
+      warnings: false,
+    },
+  };
+
+  // Minify main JavaScript files
+  pump([
+    gulp.src(["popup.js", "background.js", "content.js"]),
+    minify(options),
+    gulp.dest("dist"),
+  ]);
+
+  // Copy and minify CSS
+  pump([gulp.src("popup.css"), require("gulp-clean-css")(), gulp.dest("dist")]);
+
+  // Copy HTML and manifest
+  gulp.src(["popup.html", "manifest.json"]).pipe(gulp.dest("dist"));
+
+  // Copy icons
+  gulp.src("*.png").pipe(gulp.dest("dist"));
+
+  cb();
 });
 
-// Minify and obfuscate JavaScript using UglifyJS
-function js() {
-  return src(["popup.js", "background.js", "content.js"])
-    .pipe(
-      uglify({
-        mangle: true,
-        compress: {
-          dead_code: true,
-          drop_debugger: true,
-          conditionals: true,
-          evaluate: true,
-          booleans: true,
-          loops: true,
-          unused: true,
-          hoist_funs: true,
-          keep_fargs: false,
-          hoist_vars: true,
-          if_return: true,
-          join_vars: true,
-          cascade: true,
-          side_effects: true,
-          warnings: false,
-        },
-      })
-    )
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(dest("dist"));
-}
+gulp.task("zip", () =>
+  gulp
+    .src([
+      "dist/**",
+      "!*.zip",
+      "!node_modules/**",
+      "!node_modules/",
+      "!gulpfile.js",
+      "!package-lock.json",
+    ])
+    .pipe(zip("PageSumry.zip"))
+    .pipe(gulp.dest("."))
+);
 
-// Minify CSS
-function css() {
-  return src("styles.css")
-    .pipe(postcss([cssnano()]))
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(dest("dist"));
-}
-
-// Copy HTML and manifest
-function copy() {
-  return src(["popup.html", "manifest.json", "*.png"]).pipe(dest("dist"));
-}
-
-// Update references in HTML and manifest to minified files
-function updateRefs() {
-  return src(["dist/popup.html", "dist/manifest.json"])
-    .pipe(replace("popup.js", "popup.min.js"))
-    .pipe(replace("background.js", "background.min.js"))
-    .pipe(replace("content.js", "content.min.js"))
-    .pipe(replace("styles.css", "styles.min.css"))
-    .pipe(dest("dist"));
-}
-
-// Zip the distribution folder
-function zipDist() {
-  return src("dist/**").pipe(zip("chrome-extension.zip")).pipe(dest("."));
-}
-
-// Default task
-exports.default = series(parallel(js, css, copy), updateRefs, zipDist);
+gulp.task("default", gulp.series("compress", "zip"));
